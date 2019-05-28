74457262fb1c82afe48a674e719c352a32019ff5
Vladyslav Fedchenko <vladfedchenko@gmail.com>
2019-05-28 09:36:32 +0200

    Setting file fixes

--- settings.py
+++ settings.py
@@ -9,13 +9,13 @@ tel_channel_name = ''
 
 tel_db_dir = 'tdlib_db'
 tel_log_file = 'tdlib.log'
-tel_log_verbosity = 2
+tel_log_verbosity = 2  # WARNING level
 
 # Reddit settings
 red_client_id = ''
-red_client_secter = ''
+red_client_secret = ''
 red_username = ''
-red_password = ''
+red_password = ''  # WARNING: password is stored without encryption. Make sure to secure the file.
 red_user_agent = ''
 
 red_subreddit_name = ''
@@ -26,4 +26,4 @@ red_tmp_dir = 'tmp'
 # General
 log_location = 'reddigram_reposter.log'
 log_format_str = '%(asctime)s - %(threadName)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s'
-log_level = 30  # WARNING
+log_level = 30  # WARNING level
